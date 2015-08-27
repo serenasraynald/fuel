c3b8bdb913c38e06a1502cd78d743a004a993edd
Raynald Jabon Serenas <serenas.raynald@gmail.com>
2015-08-27 14:21:59 +0800

    initial commit

---
+++ fuel/app/logs/2015/08/27.php
@@ -0,0 +1,6 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2015-08-27 11:53:05 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2015-08-27 12:06:09 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2015-08-27 12:09:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2015-08-27 12:51:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
